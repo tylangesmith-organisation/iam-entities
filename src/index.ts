df53aae599dda899e5d1f5bd9e13065e74bc432b
Ty Lange-Smith <tylangesmith@gmail.com>
2021-05-15 17:01:33 +1000

    Debug the env var

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { App } from '@aws-cdk/core';
 import IamEntities from './stack';
 
+console.log(`BRANCH_NAME: ${process.env.BRANCH_NAME}`)
+
 const app = new App()
 new IamEntities(app, {
   stackName: `iam-entities-${process.env.BRANCH_NAME}`
