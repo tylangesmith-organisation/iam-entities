9cff0b9b20fa3c3423d0b07280f22a1ea11b6aad
Ty Lange-Smith <tylangesmith@gmail.com>
2021-05-15 16:50:21 +1000

    Try a deploy

--- deploy.sh
+++ deploy.sh
@@ -3,16 +3,16 @@
 set -euo pipefail
 
 BRANCH_NAME=${GITHUB_REF##*/}
-# TOOLKIT_STACK_NAME="cdk-toolkit-${BRANCH_NAME}-${ACCOUNT_ID}"
-# BUCKET_NAME="${TOOLKIT_STACK_NAME}-${ACCOUNT_ID}"
 
 # Install our npm dependencies
 echo "--- 🚀 Installing npm dependencies..."
 npm i
 
-# # Bootstrap the cdk toolkit
-# echo "--- 🚀 Bootstrapping CDK..."
-# npx cdk bootstrap \
-#   --toolkit-stack-name ${TOOLKIT_STACK_NAME} \
-#   --toolkit-bucket-name ${BUCKET_NAME} \
-#   aws://${ACCOUNT_ID}/${REGION}
+# Deploy the cdk stack
+echo "--- 🚀 Deploying CDK stack..."
+npx cdk \
+  --require-approval never \
+  --force \
+  --execute true \
+  --app "ts-node ./src/index.ts" \
+  deploy
