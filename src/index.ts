21b356fd61e9d73f7687c89f4930a62092d6211c
Ty Lange-Smith <tylangesmith@gmail.com>
2021-05-15 16:59:15 +1000

    Ok update the stack name

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,6 @@ import { App } from '@aws-cdk/core';
 import IamEntities from './stack';
 
 const app = new App()
-new IamEntities(app, {})
+new IamEntities(app, {
+  stackName: `iam-entities-${process.env.BRANCH_NAME}`
+})
